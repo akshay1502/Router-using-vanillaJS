95d440a0c5b2e7e73bf98203eb9b0444d798bc87
akshay shinde <akshayshinde7289@gmail.com>
2021-12-21 14:45:45 +0530

    setting up the prod static port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'));
+app.use(process.env.PORT || '/', express.static('public'));
 
 app.get('/*', (req,res) => {
   res.sendFile(__dirname+"/"+"index.html");
