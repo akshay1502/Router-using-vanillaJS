44a047d1a87af2f46e4d23e46a19bd46a6d4c42b
akshay shinde <akshayshinde7289@gmail.com>
2021-12-21 10:54:29 +0530

    static files changes

--- server.js
+++ server.js
@@ -2,11 +2,8 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
-
 app.get('/*', (req,res) => {
   res.sendFile(__dirname+"/"+"index.html");
 })
 
 app.listen(process.env.PORT || 3000);
-console.log(process.env.PORT);
